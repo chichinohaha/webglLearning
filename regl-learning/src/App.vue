47c435820747e6f9511b1c38371fe9346aecad75
chichinohaha <13622872688@163.com>
2021-08-15 22:05:25 +0800

    配置eslint 以及 regl 引擎

---
+++ regl-learning/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import { test } from './scripts/test'
+export default class App extends Vue {
+  mounted () {
+    test()
+  }
+}
+</script>
+
+<style>
+</style>
